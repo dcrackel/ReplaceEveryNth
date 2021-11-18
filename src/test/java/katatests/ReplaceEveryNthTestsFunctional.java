8468e207c5aea705e427777e5d3a90a50eaf22c5
Dwain Crackel <dcrackel@gmail.com>
2021-11-18 11:45:30 -0500

    Added unit tests for pure functions

--- src/test/java/katatests/ReplaceEveryNthTestsFunctional.java
+++ src/test/java/katatests/ReplaceEveryNthTestsFunctional.java
@@ -5,28 +5,44 @@ import kata.ReplaceEveryNthFunctional;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ReplaceEveryNthTestsFunctional {
     ReplaceEveryNthFunctional subject = null;
+    List<String> defaultNthSeq = null;
 
     @Before
     public void setUp() {
+        defaultNthSeq = new ArrayList<String>();
+        defaultNthSeq.add("a");
+        defaultNthSeq.add("o");
+        defaultNthSeq.add("a");
+        defaultNthSeq.add("o");
+        defaultNthSeq.add("a");
+    }
+
+    @Test
+    public void splitByActuallySplits(){
+        String input1 = "Vader said: No, I am your father!";
+        List<String> result =  subject.splitBy(input1, 'a');
+        assertEquals(result.size(), 5);
+        assertEquals(result.get(1), "der s");
+    }
+
+    @Test
+    public void generateEveryNthSequenceCanGenerateCorrectly(){
+        List<String> separators = subject.generateEveryNthSequence(2, "o", "a", 5);
+        assertEquals(separators.size(), 5);
+        assertEquals(separators, defaultNthSeq);
     }
 
     @Test
     public void replaceEveryNth() {
-            /* replaceNth(text, n, oldValue, newValue);
-            n:         2
-            oldValue: 'a'
-            newValue: 'o'
-            "Vader said: No, I am your father!" -> "Vader soid: No, I am your fother!"
-            1     2          3        4       -> 2nd and 4th occurence are replaced
-
-     */
         String input1 = "Vader said: No, I am your father!";
         String result = subject.replaceNth(input1, 2, 'a', 'o');
-
         assertEquals(result, "Vader soid: No, I am your fother!");
     }
 
